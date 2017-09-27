17f664800b442ad5726bc0d9f3649696ea924c50
Juutinen Samu <K9037@student.jamk.fi>
2017-09-27 10:14:37 +0300

    No idea what has happened

--- Labrat/Program.cs
+++ Labrat/Program.cs
@@ -10,7 +10,7 @@ namespace Labrat
     {
         static void Main(string[] args)
         {
-            Labrat.lab01.
+            Labrat.lab01.tehtava19();
         }
     }
 }
--- Labrat/lab01.cs
+++ Labrat/lab01.cs
@@ -8,9 +8,53 @@ namespace Labrat
 {
     class lab01
     {
-        public static void tehtava1();
-            //Tee ohjelma, joka tulostaa käyttäjän antamaa lukua (1, 2 tai 3) vastaavan luvun sanana (yksi, kaksi tai kolme).
-            //Jos käyttäjä syöttää jonkin muun luvun, tulee näytölle tulostaa teksti: "joku muu luku".
-            Console.WriteLine
-    }
+                public static void tehtava19()
+        {
+            string sana = "putkea";
+
+            
+            char[] a = new char[sana.Length];
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = '_';
+            }
+            Console.WriteLine(".....HIRSIPUU.....");
+            Console.Write("Sanasi on: ");
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.Write(a[i] + "  ");
+            }
+            Console.WriteLine();
+
+            int luku = 0;
+            do
+            {
+                Console.Write("Anna kirjain: ");
+                char input = Console.ReadLine().ToCharArray()[0];
+
+                for (int i = 0; i < sana.Length; i++)
+                {
+                    
+                    if (sana[i] == input)
+                    {
+                        luku++; 
+                        a[i] = input;  
+
+                        
+                        for (int j = 0; j < a.Length; j++)
+                        {
+                            Console.Write(a[j] + " ");
+                        }
+                    }
+                }
+                
+            }
+
+            while (luku < a.Length);
+            Console.WriteLine("You guessed it right");
+            Console.ReadLine();
+        }
+
+  }
 }
