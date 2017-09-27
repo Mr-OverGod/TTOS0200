2a14fd61b8f40b11376c112228fc596c60f2de8b
Juutinen Samu <K9037@student.jamk.fi>
2017-09-27 10:47:10 +0300

    Lab04 start

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labra04
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Profession { get; set; }
+        public int Salary { get; set; }
+
+
+        // default constructor
+        public Person()
+        {
+        }
+
+        // constructor takes person firstname and lastname as a parameter
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        // just one method what Person can do
+        public void PersonMethod()
+        {
+            Console.WriteLine("This method belongs to Person!");
+        }
+
+        // return Person data as a one string, override Object class ToString()-method
+        public override string ToString()
+        {
+            return "Name: " + FirstName + " " + LastName + " Profession: " + Profession + " Salary: " + Salary;
+        }
+
+
+
+    }
+    class Teacher : Person
+    {
+        public string Car { get; set; }
+        public string Bonus { get; set; }
+
+        // default constructor
+        public Teacher()
+        {
+        }
+
+        // parametric constuctor calls base class constructor with parameters
+        public Teacher(string firstName, string lastName, string car, string bonus)
+            : base(firstName, lastName)
+        {
+            Car = car;
+            Bonus = Bonus;
+        }
+
+        // just one method what Teacher can do 
+        public void TeacherMethod()
+        {
+            Console.WriteLine("This method belongs to Teacher!");
+        }
+
+        // override base class ToString()-method
+        public override string ToString()
+        {
+            return base.ToString() + " Car: " + Car + " Bonus: " + Bonus;
+        }
+    }
+
+
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labra04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                
+
+                // create a one Person object
+                Person person = new Person();
+                person.FirstName = "Kirsi";
+                person.LastName = "Kernel";
+                person.Salary = 2000;
+                person.Profession = "Teacher";
+                Console.WriteLine(person.ToString());
+
+                // create a one Teacher Object
+                Teacher teacher = new Teacher();
+                teacher.FirstName = "Teppo";
+                teacher.LastName = "Konsoli";
+                teacher.Profession = "Head of Institute";
+                teacher.Salary = 4000;
+                teacher.Car = "S6";
+            teacher.Bonus = "2000";
+                Console.WriteLine(teacher.ToString());
+
+
+                // do something methods
+                person.PersonMethod(); // This method belongs to Person!
+                teacher.PersonMethod(); // This method belongs to Person! (derived class can use it!)
+                teacher.TeacherMethod(); // This method belongs to Teacher!
+            }
+        }
+    }
+    
+
