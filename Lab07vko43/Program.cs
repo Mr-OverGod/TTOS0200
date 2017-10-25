8a1c1968d9dc27b84ff796a7d03f005fb993c630
Juutinen Samu <K9037@student.jamk.fi>
2017-10-25 10:19:02 +0300

    task1

---
+++ Lab07vko43/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nimi;
+            string filupath = @"D:\K9037";
+            //luodaan Streamwriter olio johon kirjoitetaan
+            StreamWriter sw = new StreamWriter(filupath + @"\testi.txt");
+            do
+            {
+                Console.Write("Anna nimi (Enter lopettaa): ");
+                nimi = Console.ReadLine();
+                if (nimi.Length > 0)
+                    sw.WriteLine(nimi);
+            } while (nimi.Length != 0);
+            sw.Close();
+        }
+    }
+}
